0975a77a33ea63636d8644e4640d71c6cd8a6d7b
dreadn0ught <31293924+dreadn0ught@users.noreply.github.com>
2025-08-08 16:00:10 +0000

    Add dockerfile demo working v0.1

--- docker/safe/build.sh
+++ docker/safe/build.sh
@@ -1,1 +1,6 @@
-docker build --no-cache --progress=plain -t safe-python-app .
+#!/bin/bash
+
+printf '%s' "$OSSPREY_API_KEY" | docker buildx build \
+  --no-cache --progress=plain \
+  --secret id=ossprey_api_key,src=/dev/stdin \
+  -t safe-python-app .
