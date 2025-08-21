5c174fb828397ea06a02970225bbe9e835e23f98
dreadn0ught <31293924+dreadn0ught@users.noreply.github.com>
2025-08-21 09:30:20 +0000

    fixed docker scan

--- docker/malicious_python/build.sh
+++ docker/malicious_python/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-printf '%s' "$OSSPREY_API_KEY" | docker buildx build \
+printf '%s' "$OSSPREY_QA_API_KEY" | docker buildx build \
   --no-cache --progress=plain \
   --secret id=ossprey_api_key,src=/dev/stdin \
   -t safe-python-app .
